b60d88ed1314ea0e0bc822b73c1e3f740c2022ed
llyndev <weslley.alves3310@gmail.com>
2024-11-13 14:46:49 -0300

    feat: Faculdade POO

---
+++ src/Calculadora.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+
+        int numero;
+        double num, num2, res;
+
+        System.out.println("Qual operação deseja realizar?: ");
+        System.out.println("1 - ADIÇÃO");
+        System.out.println("2 - SUBTRAÇÃO");
+        System.out.println("3 - MULTIPLICAÇÃO");
+        System.out.println("4 - DIVISÃO");
+        System.out.println("Para sair digite qualquer numero");
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Digite: ");
+        numero = teclado.nextInt();
+
+        System.out.print("Digite um valor: ");
+        num = teclado.nextInt();
+
+        System.out.print("Digite outro valor: ");
+        num2 = teclado.nextInt();
+
+        teclado.close();
+
+        if (numero == 1) {
+            res = num + num2;
+            System.out.printf("A soma de %.0f + %.0f = %.0f", num, num2, res);
+        } else if (numero == 2) {
+            res = num - num2;
+            System.out.printf("A subtração de %.0f + %.0f = %.0f", num, num2, res);
+        } else if (numero == 3) {
+            res = num * num2;
+            System.out.printf("A multiplicação de %.0f + %.0f = %.0f", num, num2, res);
+        } else if (numero == 4) {
+            res = num / num2;
+            System.out.printf("A divisão de %.0f + %.0f = %.2f", num, num2, res);
+        } else {
+            System.out.print("Fim");
+        }
+    }
+}
---
+++ src/IMC.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class IMC {
+
+    public static void main(String[] args) {
+
+        int peso;
+        double altura, imc;
+        String nome;
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Informe seu nome: ");
+        nome = teclado.next();
+
+        System.out.print("Informe o peso: ");
+        peso = teclado.nextInt();
+
+        System.out.print("Informe a altura: ");
+        altura = teclado.nextDouble();
+
+        teclado.close();
+
+        imc = peso / (altura * altura);
+
+        System.out.printf("Nome: %s\nPeso: %d\nAltura: %.2f\nIMC: %.2f", nome, peso, altura, imc);
+
+    }
+}
